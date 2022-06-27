08db59a0c005c10b4e1774fd3188567e31d56549
chaitanya005 <muvvalachaitanya05@gmail.com>
2022-06-27 17:12:34 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+const ohlcDataPts = require("./routes/ohlcDataPoints");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", ohlcDataPts);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "HElloo!!",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening: http://localhost:${port}`);
+});
+
+module.exports = app;
---
+++ controllers/graphDataPoints.js
@@ -0,0 +1,59 @@
+const { default: axios } = require("axios");
+const moment = require("moment");
+
+async function getDataPoints(req, res) {
+  try {
+    const { basketData } = req.body;
+    let growthRatePercentage = [],
+      timeStamp;
+    for (let i = 0; i < basketData.length; i++) {
+      await axios
+        .get(
+          `https://api.coingecko.com/api/v3/coins/${basketData[i].name}/ohlc?vs_currency=usd&days=${basketData[i].noOfDays}`
+        )
+        .then((result) => {
+          let coinsObj = {
+            [basketData[i].name]: result.data.map((item) => ({ ...item })),
+          };
+
+          let growthRate = coinsObj[basketData[i].name].map(
+            (item) =>
+              ((((item["4"] - item["1"]) * 100) / item["1"]) *
+                [basketData[i].ratio]) /
+              100
+          );
+
+          let timeStamps = coinsObj[basketData[i].name].map(
+            (item) => item["0"]
+          );
+          timeStamp = timeStamps;
+          growthRatePercentage.push(growthRate);
+        });
+    }
+    var sum = (r, a) =>
+      r.map((b, i) => {
+        let num = a[i] + b;
+        return Number(num.toFixed(5));
+      });
+
+    let totalGrowthRates = growthRatePercentage.reduce(sum);
+    let array = [];
+    const data = (a1, a2) =>
+      a1.map((a, i) => {
+        let obj = {
+          point: a,
+          timeStamp: moment(a2[i]).format("MMM Do YYYY hh:mm a"),
+        };
+        array.push(obj);
+      });
+    data(totalGrowthRates, timeStamp);
+    res.status(200).json(array);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: "Error" });
+  }
+}
+
+module.exports = {
+  getDataPoints,
+};
